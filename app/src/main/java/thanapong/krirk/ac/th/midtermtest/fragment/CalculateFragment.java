a1b4e63dbd130d78946b4f7c10da4b65445b9c50
cyber01 <tnptnpj37@gmail.com>
2018-02-12 22:09:07 -0800

    finish test

--- app/src/main/java/thanapong/krirk/ac/th/midtermtest/fragment/CalculateFragment.java
+++ app/src/main/java/thanapong/krirk/ac/th/midtermtest/fragment/CalculateFragment.java
@@ -1,8 +1,88 @@
 package thanapong.krirk.ac.th.midtermtest.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import thanapong.krirk.ac.th.midtermtest.MainActivity;
+import thanapong.krirk.ac.th.midtermtest.R;
+
 /**
- * Created by cyber01 on 2/12/2018.
+ * Created by acer on 9/1/2561.
  */
 
-public class CalculateFragment {
-}
+public class CalculateFragment extends Fragment implements View.OnClickListener {
+    //    Explicit
+
+
+    private String displayString = "";
+
+    private String tag = "23JanV1";
+    private  boolean addABoolean = false, minusAboolean = false;
+
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+
+        createToolbar();
+
+
+
+
+
+    } // Main Method
+
+
+
+
+
+
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setTitle("Calculate USD To THB");
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragmentcal, container, false);
+        return view;
+    }
+
+
+
+
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}  //Main class
--- app/src/main/java/thanapong/krirk/ac/th/midtermtest/fragment/MainFragment.java
+++ app/src/main/java/thanapong/krirk/ac/th/midtermtest/fragment/MainFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import thanapong.krirk.ac.th.midtermtest.R;
 
@@ -14,7 +15,40 @@ import thanapong.krirk.ac.th.midtermtest.R;
  * Created by cyber01 on 2/12/2018.
  */
 
+
+
+
+
 public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+
+//        Calculate Controller
+        calculateController();
+
+    }   // Main Method
+
+    private void calculateController() {
+
+        Button button = getView().findViewById(R.id.btncalculate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new CalculateFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+
+    }
 
 
     @Nullable
